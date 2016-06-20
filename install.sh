5e1d1a9c65b47127caea931e8b60fa3d2a9aac9b
takanashi66 <takanashi66@gmail.com>
2016-06-21 00:23:21 +0900

    ディレクトリ構造変更

--- install.sh
+++ install.sh
@@ -1,20 +1,23 @@
 #!/bin/sh
 
-mkdir html;
-cd html;
-mkdir common;
-cd common;
-mkdir css;
-mkdir js;
-mkdir img;
+cd /Users/takanashi66/Dropbox/workspace/web/
 
-cd ../../;
-mkdir src;
-cd src;
+echo -n "What is file name? : "
+read name
+mkdir $name
+cd $name
+mkdir common
+mkdir common/css
+mkdir common/img
+mkdir common/js
 
+mkdir src
+cd src
 compass create --sass-dir "sass" --output-style "compressed" --relative-assets --no-line-comments --bare
 
-sudo npm install --save-dev gulp browser-sync gulp-compass gulp-plumber;
+mkdir js
+
+sudo npm install --save-dev gulp browser-sync gulp-compass gulp-plumber gulp-combine-media-queries;
 
 git clone git@bitbucket.org:takanashi66/gulp_task.git;
 cp gulp_task/gulpfile.js ./;
@@ -24,3 +27,5 @@ cd ../
 
 git init;
 
+echo .* > .gitignore
+echo src/node_module >> .gitignore
