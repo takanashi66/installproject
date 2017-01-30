671034ea329e6ceb6fded1c7ab2d2a176ef86341
takanashi66 <inoue@codecodeweb.com>
2017-01-30 21:59:56 +0900

    修正

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ cp gulp_task/gulpfile.js ./
 cp gulp_task/package.json ./
 rm -rf gulp_task
 
-npm install --save-dev gulp browser-sync gulp-plumber gulp-sass gulp-notify gulp-autoprefixer gulp-sourcemaps gulp-merge-media-queries gulp-minify-css gulp-uglify gulp-minify-html
+npm install --save-dev gulp browser-sync gulp-plumber gulp-sass gulp-notify gulp-autoprefixer gulp-sourcemaps gulp-merge-media-queries gulp-minify-css gulp-uglify gulp-minify-html gulp-concat
 
 git init;
 
